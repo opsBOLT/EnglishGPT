898aa012cffbc7ef92cfcb8fee7db5a73bf43976
opsBOLT <phantomgaming146@gmail.com>
2025-11-24 17:28:46 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Geist"', 'sans-serif'],
+        sans: ['"Shantell Sans"', 'cursive'],
       },
       colors: {
         primary: {
